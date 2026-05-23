2f6984adcee464e3daeb1b99aa536192c80775e8
wolfuy <phuhoparttime155@gmail.com>
2026-05-24 04:11:56 +0700

    Khởi tạo Project WPF và chia cấu trúc thư mục

---
+++ TimeManagement/TimeManagement/Services/AI_Services.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeManagement.Services
+{
+    internal class AI_Services
+    {
+    }
+}
